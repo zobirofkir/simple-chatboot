da5bcebe9da42d146ec48bd04cf88c52d4b25514
zobirofkir <zobirofkir19@gmail.com>
2025-03-19 02:08:42 +0000

    updaate

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/chat', [ChatbotController::class, 'index'])->name('chat');
     Route::post('/chat', [ChatbotController::class, 'store']);
 });
-
-Route::middleware(['auth'])->group(function () {
-    Route::post('/chats/create', [ChatController::class, 'create']);
-    Route::get('/chats/{chat}', [ChatController::class, 'show']);
-    Route::post('/chat', [ChatController::class, 'sendMessage']);
-});
-
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
